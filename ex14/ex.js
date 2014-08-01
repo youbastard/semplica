e2b0aca127e96b85a18041c73e0ab432a5122ac5
Nicholas Ortenzio <nicholas.ortenzio@gmail.com>
2014-08-01 13:22:16 -0400

    chord diagram

---
+++ ex14/ex.js
@@ -0,0 +1,39 @@
+// ex14
+// Chord Diagram example
+
+// original example: http://bl.ocks.org/mbostock/4062006
+
+add.controller('ex14', function ($scope, $http) {
+
+  var matrix = [
+    [11975,  5871, 8916, 2868],
+    [ 1951, 10048, 2060, 6171],
+    [ 8010, 16145, 8090, 8045],
+    [ 1013,   990,  940, 6907]
+  ];
+
+  var width = 960;
+  var height = 500;
+  var innerRadius = Math.min(width, height) * 0.41;
+  var outerRadius = innerRadius * 1.1;
+
+  var chord = d3.layout.chord()
+      .padding(0.05)
+      .sortSubgroups(d3.descending)
+      .matrix(matrix);
+
+  $scope.groups = chord.groups();
+  $scope.chords = chord.chords();
+  $scope.fill = d3.scale.ordinal().domain(d3.range(4)).range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+  $scope.arc = d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+  $scope.radius = d3.svg.chord().radius(innerRadius);
+
+  $scope.onMouseOver = function (e) {
+    console.log('mouseover');
+  };
+
+  $scope.onMouseLeave = function (e) {
+    console.log('onmouseleave');
+  };
+
+});
